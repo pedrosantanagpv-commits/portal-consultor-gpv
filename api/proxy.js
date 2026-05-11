ae0d61ff39f6c477c350877a5b79ff9972991bf7
pedrosantanagpv-commits <pedrosantanagpv@gmail.com>
2026-05-11 17:02:56 -0300

    Update fetch URL to use APPS_SCRIPT_URL

--- api/proxy.js
+++ api/proxy.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
 
   try {
 
-    const resposta = await fetch(SCRIPT_URL, {
+    const resposta = await fetch(APPS_SCRIPT_URL,{
       method: "POST",
       headers: {
         "Content-Type": "text/plain;charset=utf-8"
